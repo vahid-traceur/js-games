ec20d0eff88f0cb7edb8aad33f015ebe2123c0e8
vahid-traceur <vahidesnaashari79@gmail.com>
2021-12-28 20:53:56 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,169 @@
+// Canvas setup
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
+
+// Mouse interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    click: false
+}
+canvas.addEventListener('mousedown', function (e) {
+    mouse.click = true;
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseup', function (e) {
+    mouse.click = false;
+})
+
+// Player
+const playerLeft = new Image();
+playerLeft.src = 'assets/fish/swim_fish_left.png';
+const playerRight = new Image();
+playerRight.src = 'assets/fish/swim_fish_right.png';
+
+class Player {
+    constructor() {
+        this.x = canvas.width;
+        this.y = canvas.height / 2;
+        this.radius = 50;
+        this.angle = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.frame = 0;
+        this.spriteWidth = 498;
+        this.spriteHeight = 327;
+        console.log('animate: ', this);
+    }
+
+    update() {
+        const dx = this.x - mouse.x;
+        const dy = this.y - mouse.y;
+        let theta = Math.atan2(dy, dx);
+        this.angle = theta;
+        if (mouse.x !== this.x) {
+            this.x -= dx / 15;
+        }
+        if (mouse.y !== this.y) {
+            this.y -= dy / 15;
+        }
+    }
+
+    draw() {
+        if (mouse.click) {
+            ctx.lineWidth = 0.2;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        if (this.x >= mouse.x) {
+            ctx.drawImage(playerLeft, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
+                this.spriteWidth, this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth / 4, this.spriteHeight / 4);
+        } else {
+            ctx.drawImage(playerRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
+                this.spriteWidth, this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth / 4, this.spriteHeight / 4);
+        }
+        ctx.restore();
+    }
+}
+
+const player = new Player();
+
+
+// Bubbles
+const bubblesArray = [];
+
+class Bubble {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 100;
+        this.radius = 50;
+        this.speed = Math.random() * 5 + 1;
+        this.distance;
+        this.counted = false;
+        this.sound = Math.random() <= .5 ? 'sound1' : 'sound2';
+    }
+
+    update() {
+        this.y -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy);
+    }
+
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const bubblePop1 = document.createElement('audio');
+bubblePop1.src = 'sounds/Plop.ogg';
+const bubblePop2 = document.createElement('audio');
+bubblePop2.src = 'sounds/bubbles-single2.wav';
+
+function handleBubbles() {
+    if (gameFrame % 50 === 0) {
+        bubblesArray.push(new Bubble());
+    }
+    for (let i = 0; i < bubblesArray.length; i++) {
+        bubblesArray[i].update();
+        bubblesArray[i].draw();
+        if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
+            bubblesArray.splice(i, 1);
+            i--;
+        } else if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+            if (!bubblesArray[i].counted) {
+                // if (bubblesArray[i].sound === 'sound1')
+                // bubblePop1.play();
+                // else
+                // bubblePop2.play();
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i, 1);
+                i--;
+            }
+        }
+
+    }
+}
+
+// Animation loop
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleBubbles();
+    player.update();
+    player.draw();
+    ctx.fillStyle = 'black';
+    ctx.fillText('score: ' + score, 10, 50);
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener('resize', function () {
+    canvasPosition = canvas.getBoundingClientRect();
+});
